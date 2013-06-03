f365b17cae4be941d9f4109f943791f26992035c
Robert Haritonov <r@rhr.me>
2013-06-03 11:13:52 +0300

    Merge with master

--- user/js/options.js
+++ user/js/options.js
@@ -12,31 +12,35 @@ define({
             //trimSpaces: true
         },
 
+        modulesOptions : {
+            //Modules options example
+            //innerNavigation : {
+            //  hashSymb: '!'
+            //  }
+        },
+
         // Plugins
         pluginsEnabled : {
             //Plugins enabling
             //debugmode: true,
 
-            //PHP and proper chmod for file writing required for these plugins
-            //bubble: true,
-            debugmode: true,
-            globalNav: true,
-            search: true
+            //PHP and proper chmod for file writing required for bubble (comments) plugin
+            //bubble: true
         },
 
         pluginsOptions : {
             //Plugins options example
-//            globalNav : {
-//                pageLimit: 999
-//            }
+            //globalNav : {
+            //    pageLimit: 999
+            //}
         }
     },
 
     // Default options for landing page
     landingOptionsExceptions : {
         pluginsEnabled : {
-            bubble: false,
-            debuggmode: false
+            //bubble: false,
+            //debuggmode: false
         }
     }
 
